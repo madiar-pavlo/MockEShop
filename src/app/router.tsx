9c4bf44567bb10fde34b5c7503e9c5bc0104b6bf
Anonim <nelubitelzmej@gmail.com>
2025-02-01 03:29:12 +0100

    BUILD

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -3,7 +3,7 @@ import { createBrowserRouter } from 'react-router-dom';
 
 export const router = createBrowserRouter([
   {
-    path: '/',
+    path: '*',
     element: <App />,
     children: [
       {
