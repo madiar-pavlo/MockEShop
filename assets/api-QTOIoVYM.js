c64e30bed89d07ea2c5ec87ed662fadf402648ee
Anonim <nelubitelzmej@gmail.com>
2025-02-01 02:11:53 +0100

    Updates

--- assets/api-QTOIoVYM.js
+++
@@ -1,1 +0,0 @@
-import{a6 as r}from"./index-CXUv5O_L.js";const s=r.injectEndpoints({endpoints:e=>({getUser:e.query({query:()=>({method:"getUser"})})}),overrideExisting:!0}),{useGetUserQuery:i}=s;export{i as u};
---
+++ assets/useAppContext-D-fm53ag.js
@@ -0,0 +1,1 @@
+import{i as e}from"./index-BvJZQbNX.js";const n=o=>{const t=e.useContext(o);if(t===void 0)throw new Error("context dos not exist");return t};export{n as u};
