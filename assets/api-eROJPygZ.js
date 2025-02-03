f5456c04ec08a6910f8b5bec92eafa5fa3554e81
Anonim <nelubitelzmej@gmail.com>
2025-02-03 18:20:27 +0100

    Updates

---
+++ assets/api-eROJPygZ.js
@@ -0,0 +1,1 @@
+import{a6 as r}from"./index-LTdjeOPP.js";const s=r.injectEndpoints({endpoints:e=>({getUser:e.query({query:()=>({method:"getUser"})})}),overrideExisting:!0}),{useGetUserQuery:i}=s;export{i as u};
