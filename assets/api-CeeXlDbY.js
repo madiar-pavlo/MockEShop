9c25f28ed42dba06ae6ae61945203b82e39f451b
Anonim <nelubitelzmej@gmail.com>
2025-02-08 20:42:14 +0100

    Updates

--- assets/api-CeeXlDbY.js
+++
@@ -1,1 +0,0 @@
-import{a6 as r}from"./index-DyyJb0Wj.js";const s=r.injectEndpoints({endpoints:e=>({getUser:e.query({query:()=>({method:"getUser"})})}),overrideExisting:!0}),{useGetUserQuery:i}=s;export{i as u};
