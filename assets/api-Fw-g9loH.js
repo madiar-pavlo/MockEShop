5780cfa77cc27f801f21fb7b1fe877b8bef0830f
Anonim <nelubitelzmej@gmail.com>
2025-02-08 23:02:06 +0100

    Updates

--- assets/api-Fw-g9loH.js
+++
@@ -1,1 +0,0 @@
-import{a6 as r}from"./index-BUJwxagW.js";const s=r.injectEndpoints({endpoints:e=>({getUser:e.query({query:()=>({method:"getUser"})})}),overrideExisting:!0}),{useGetUserQuery:i}=s;export{i as u};
